f59fbde42f04fc5bb8d0baf8d678248de40307f8
d.plawecki <dar.plawecki@gmail.com>
2014-11-11 00:56:24 +0100

    Parę kolejnych przykładów - błędy, referer, przekierowania, statusy HTTP i notatki

--- wsgi_app.py
+++ wsgi_app.py
@@ -10,20 +10,24 @@ def content(environ):
     #global user_headers 
     #user_headers = [('Country', 'Poland')] #W ten sposób ustawiamy nagłówki użytkownika
     #user_headers = [('Set-Cookie', 'ciastko=suchar')] #W ten sposób ustawiamy ciastko
+    #user_headers = [('Location', 'http://google.pl')] #W ten sposób możemy zrobić przekierowanie - działa z odpowiednimi kodami HTTP(301,302,307 itp)
 
     #return environ.get('QUERY_STRING') ## W ten sposób dostajemy GET'a
     #return environ.get('PATH_INFO') ## a w ten ścieżkę - na podstawie tego można zbudować routing
     #return environ.get('REQUEST_URI') ## a w ten jedno i drugie
     #return environ.get('HTTP_HOST') ##a w ten nazwę hosta
     #return environ.get('REQUEST_METHOD')  ## a w ten metodę - na podstawie tego można zbudować testy isPost(), isGet() itp...
-    return repr(environ['wsgi.input'].read()) ## w ten sposób można dostać surowy request HTTP -- na tej podstawie można sobie zbudować POSTa
+    #return repr(environ['wsgi.input'].read()) ## w ten sposób można dostać surowy request HTTP -- na tej podstawie można sobie zbudować POSTa
     #return environ.get('HTTP_USER_AGENT') ## w ten sposób(HTTP_*) pobieramy nagłówki, np. "User Agent"
     #return environ.get('HTTP_COOKIE') ## Przyklad z Cookie
+    #return environ.get('HTTP_REFERER') ## Przyklad z Refererem
     #if 'HTTP_X_REQUESTED_WITH' in environ:
     #    return environ.get('HTTP_X_REQUESTED_WITH') ## Przyklad sprawdzający czy to Ajax
     ##return environ.get('REMOTE_ADDR') ## w ten sposób pobieramy adres klienta
     ##return environ.get('DOCUMENT_ROOT') ## w ten sposób pobieramy Docroot
-   
+  
+    ##environ['wsgi.errors'].write('Dupa!!') ##W ten sposób możemy rzucać błędami(zapisują się w logach serwera)
+
     return 'Ala ma kota'
 
 ####WSGI MAGIC#####
@@ -31,6 +35,10 @@ def content(environ):
 def wsgi_app(environ, start_response):
     global user_headers
     status = '200 OK'
+    #status = '404 Not Found' #404
+    #status = '301 Moved Permanently'
+    #status = '302 Found'
+    #status = '307 Temporary Redirect'
     output = init(environ)
     headers = [ ('Content-type', 'text/html'), ('Content-Length', str(len(output)))] + user_headers
     start_response(status, headers)
